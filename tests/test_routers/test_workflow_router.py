703b8a8780675fe50dc43af30e545944e17241f7
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-21 16:19:31 +0300

    Add drop db after each test

--- tests/test_routers/test_workflow_router.py
+++ tests/test_routers/test_workflow_router.py
@@ -12,7 +12,6 @@ from services.workflow import WorkflowServices
 DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(DATABASE_URL)
-Base.metadata.create_all(bind=engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 client = TestClient(app)
@@ -39,6 +38,8 @@ def test_create_workflow(workflow_services, db_session):
 
 
 def test_get_workflow(workflow_services, db_session):
+    Base.metadata.create_all(bind=engine)
+
     workflow_data = WorkflowCreateSchema(name="Test Workflow")
     created_workflow = workflow_services.create_workflow(workflow_data, db_session)
 
@@ -47,26 +48,33 @@ def test_get_workflow(workflow_services, db_session):
 
     assert response.status_code == 200
     assert response.json()["name"] == workflow_data.name
+    Base.metadata.drop_all(bind=engine)
 
 
 def test_update_workflow(workflow_services, db_session):
+    Base.metadata.create_all(bind=engine)
     workflow_data = WorkflowCreateSchema(name="Test Workflow")
-    created_workflow = workflow_services.create_workflow(workflow_data, db_session)
+    create_url = app.url_path_for("create_workflow")
+    create_response = client.post(create_url, json=workflow_data.dict())
     update_workflow = WorkflowUpdateSchema(name="New Workflow")
 
-    get_url = app.url_path_for("update_workflow", workflow_id=created_workflow.id)
+    get_url = app.url_path_for(
+        "update_workflow", workflow_id=create_response.json()["id"]
+    )
     response = client.put(get_url, json=update_workflow.dict())
 
     assert response.status_code == 200
     assert response.json()["name"] == update_workflow.name
-    assert response.json()["id"] == created_workflow.id
+    Base.metadata.drop_all(bind=engine)
 
 
 def test_delete_workflow(workflow_services, db_session):
+    Base.metadata.create_all(bind=engine)
     workflow_data = WorkflowCreateSchema(name="Test Workflow")
     created_workflow = workflow_services.create_workflow(workflow_data, db_session)
 
-    get_url = app.url_path_for("delete_workflow", workflow_id=created_workflow.id)
+    get_url = app.url_path_for("delete_workflow", workflow_id=1)
     response = client.delete(get_url)
 
     assert response.status_code == 204
+    Base.metadata.drop_all(bind=engine)
